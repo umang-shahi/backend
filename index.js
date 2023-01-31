2984f515a6b208f7b19fd5971ba658162d5dba3c
umang <umangshahi800@gmail.com>
2023-01-31 17:06:50 +0545

    server

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
-const myFunc = (a,b) => {
-    return a/b;
 
-}
+const http = require("http");
+const fs = require("fs")
+const hostname = "localhost";
+const PORT = 5000;
 
-console.log(myFunc(9,3));
+const home =fs.readFileSync("./home.html");
 
+const server = http.createServer((req,res)=>{
+    console.log(req.url);
 
-const app = (a,b) => {
-    return a-b;
-}
-console.log(app(2,1));
+    if(req.url === "/"){
+     return res.end(home)
+    }
 
+    else if(req.url === "/about"){
+        return res.end("<h1>About page</h1>")
+    }
+    else 
+    {
+        return res.end("<h1>404 not found</h1>")
+    }
+})
+
+server.listen(PORT,hostname,()=>{
+    console.log(`server is running at port: http://${hostname}:${PORT}`);
+
+})
