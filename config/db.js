bdfbe093e4d1a04113bf5f8a33a7634dff4fc747
umang <umangshahi800@gmail.com>
2023-02-05 17:08:40 +0545

    db

---
+++ config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+mongoose.set("strictQuery",true);
+
+const ConnectionDB = async() =>{
+    try {
+       const {connection} = await mongoose.connect(process.env.MONGODB_URL)
+        console.log(`Mongodb is connected at:${connection.host}`.cyan.underline.bold)
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+        
+    }
+}
+
+
+module.exports = ConnectionDB;
--- index.js
+++ index.js
@@ -1,33 +1,35 @@
 const express = require("express");
 const path = require("path");
-// const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", true);
+const dotenv = require('dotenv');
+const colors = require("colors");
+const ConnectionDB = require("./config/db");
+
+
+
+
+
+
+
+//configure
+
+dotenv.config();
+
+
+
+
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-//http method
-app.get("/", (req, res) => {
-  //   res.sendFile(path.join(__dirname + "/home.html"));
-});
-app.post("/api/v1/login", (req, res) => {
-  const userName = req.body.name;
-  const userEmail = req.body.email;
-  const userPassword = req.body.password;
-  res.json({
-    success: true,
-    name: userName,
-    email: userEmail,
-    password: userPassword,
-  });
-});
+
+
+
+
+ConnectionDB();
+
 //port
-const PORT = 5000;
+const PORT =  process.env.PORT;
 app.listen(PORT, () => {
-  console.log(`Server is running at port: http://localhost:${PORT}`);
+  console.log(`Server is running at port: http://localhost:${PORT}`.cyan.underline.bold);
 });
-
-
-
-
-
-
-
